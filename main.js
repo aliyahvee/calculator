0c4dcfa0fa5098d2116faa017368373a90c530d6
Aliyah Veal <Aliyah@Aliyahs-MBP-2.home>
2018-04-09 17:40:07 -0400

    added files

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var budget = parseFloat($(".total_budget").find("span").text());
+
+    $(".department input").on("keyup keydown keypress change", function(e){
+      var new_budget = budget;
+
+
+      $(".department input").each(function(){
+        var value = $(this).val();
+
+        if (value !== "") {
+            value = parseFloat(value);
+        } else{
+          value=0;
+        }
+
+        new_budget = new_budget - value;
+      });
+
+      $(".total_budget").find("span").text(new_budget);
+
+      if (new_budget < 0) {
+          alert(document.overbudget.style.color='ff0000');
+        // alert("You're over budget!");
+      }
+  });
+});
